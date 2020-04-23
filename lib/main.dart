daa6c299c8040ab29058ffe29437a3f448c389fa
Felipe Aurelio <felipeaureliotesta@gmail.com>
2020-04-22 22:31:34 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'COVID19 App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
