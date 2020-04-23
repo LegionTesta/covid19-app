5e1bdb3282f17240382bf3bc1fbb864dffc03f84
Felipe Aurelio <felipeaureliotesta@gmail.com>
2020-04-23 12:40:47 -0300

    Minor changes.

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,6 +6,6 @@ const cBackgroundColor1 = Color(0xFF3383CD);
 const cBackgroundColor2 = Color(0xFF11249F);
 
 const cInfoCardStyle = TextStyle(
-  fontSize: 20,
+  fontSize: 18,
   fontWeight: FontWeight.bold
 );
